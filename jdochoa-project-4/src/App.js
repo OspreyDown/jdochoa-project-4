844d02760cff8a106b09950877c41187d9e26eb0
OspreyDown <jdochoa@madisoncollege.edu>
2025-08-14 01:00:31 -0500

    modified app.js to display my name

--- jdochoa-project-4/src/App.js
+++ jdochoa-project-4/src/App.js
@@ -6,8 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p src={logo} className="App-logo">
+          HELLO WORLD! This is my first React app.
+          MY NAME IS JDOCHOA.
+          I am learning React and this is my first project!!!!!!!!!!!!
         </p>
         <a
           className="App-link"
